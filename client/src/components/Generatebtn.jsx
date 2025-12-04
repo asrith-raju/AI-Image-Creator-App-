90b8c4aafcbe38b7e2dcf91130d63e9bde328dee
asrith-raju <asrithraju143@gmail.com>
2025-12-04 18:53:57 +0530

    client(fix): fix generate button size

--- client/src/components/Generatebtn.jsx
+++ client/src/components/Generatebtn.jsx
@@ -6,7 +6,7 @@ const Generatebtn = () => {
     <div className='pb-16 text-center'>
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py16'>See the magic. Try now</h1>
 
-      <button className='inline-flex items-center gap-2 px-2 py-3 rounded-full bg-black text-white m-auto hover:scale-105 translate-all duration-500'>Generate Images 
+      <button className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 translate-all duration-500'>Generate Images 
         <img src={assets.star_group} alt="" className='h-6' />
       </button>
     </div>
