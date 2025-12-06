784144ae4219a5d715d3372e2e44b76e12cfc8f4
asrith-raju <asrithraju143@gmail.com>
2025-12-06 18:56:04 +0530

    client(feat): add animations to result and buy pages

--- client/src/components/Generatebtn.jsx
+++ client/src/components/Generatebtn.jsx
@@ -25,7 +25,7 @@ const Generatebtn = () => {
       className='pb-16 text-center'>
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py16'>See the magic. Try now</h1>
 
-      <button className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 translate-all duration-500'>Generate Images
+      <button onClick={onClickHandler} className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 translate-all duration-500'>Generate Images
         <img src={assets.star_group} alt="" className='h-6' />
       </button>
     </motion.div>
