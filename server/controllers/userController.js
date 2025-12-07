90def53c1d783a2262246cd50d4755c984a4aa63
asrith-raju <asrithraju143@gmail.com>
2025-12-07 19:06:15 +0530

    server(feat): create user schema

---
+++ server/controllers/userController.js
@@ -0,0 +1,10 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+    } catch (error) {
+        
+    }
