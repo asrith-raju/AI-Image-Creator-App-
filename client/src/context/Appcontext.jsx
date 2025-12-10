8e89ca85e51d697dfc2e8b62f1aae7b5fb6b39cb
asrith-raju <asrithraju143@gmail.com>
2025-12-10 17:21:59 +0530

    client(fix): add loadCreditsdata function to Appcontext

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -1,4 +1,7 @@
-import { createContext,useState} from "react";
+import axios from "axios";
+import { createContext,useEffect,useState} from "react";
+import {toast} from 'react-toastify'
+import axios from "axios";
 
 export const AppContext = createContext()
 
@@ -11,9 +14,30 @@ const AppContextProvider = (props)=>{
 
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const loadCreditdata = async()=>{
+        try {
+            const {data}= await axios.get(backendUrl+'/api/user/credits',{
+                headers:{ token}
+            })
+            if(data.success){
+                setCredit(data.credits)
+                setuser(data.user)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        if(token){
+            loadCreditdata()
+        }
+    })
     
     const value={
-        user,setuser,showLogin,setshowLogin, backendUrl,token,setToken,credit,setCredit
+        user,setuser,showLogin,setshowLogin, backendUrl,token,setToken,credit,setCredit,loadCreditdata
     }
 
     return(
