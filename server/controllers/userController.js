3d9b5c5d55c66aa2dac8db40351a3a228922224d
asrith-raju <asrithraju143@gmail.com>
2025-12-13 19:00:17 +0530

    server(feat): create route for verifyRazorpay

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -170,4 +170,4 @@ const verifyRazorpay = async (req, res) => {
      
 
 
-export { registerUser, loginUser,userCredits ,paymentRazorpay};
+export { registerUser, loginUser,userCredits ,paymentRazorpay, verifyRazorpay};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser,userCredits, paymentRazorpay } from '../controllers/userController.js';
+import { registerUser, loginUser,userCredits, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js';
 import  userauth from  '../middlewares/auth.js';
 
 const userRouter = express.Router();
@@ -8,5 +8,7 @@ userRouter.post('/register',registerUser);
 userRouter.post('/login',loginUser);
 userRouter.get('/credits',userauth,userCredits);
 userRouter.post('/pay-razor',userauth,paymentRazorpay);
+userRouter.post("/verify-razor", verifyRazorpay);
+
 
 export default userRouter;
